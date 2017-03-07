84f103c1a5c8bbb4702e987f8b17fedd7ab20c28
Petar Kolaric <petarkolaric@users.noreply.github.com>
2017-03-07 18:41:21 +1100

    styling changes, add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header'
 import UploadForm from './components/UploadForm'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header/>
         <UploadForm/>
+        <Footer/>
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = (props) => {
+  return (
+      <footer className="Footer">
+        <a href="www.github.com/petarkolaric/jomelify-frontend">Check me out on Github</a>
+        <a href="www.github.com/chrisfy">Check the original backend compne blah blah</a>
+        <a href="www.github.com/petarkolaric/jomelface">Check the server component</a>
+      </footer>
+  )
+}
+
+export default Footer
